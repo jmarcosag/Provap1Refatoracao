ac2bbce9a56e2ab25f55be0985196cedb140a9a1
João Marcos <60902889+jmarcosag@users.noreply.github.com>
2022-04-05 21:42:52 -0300

    fix: score refactor v1.1

--- tennis.py
+++ tennis.py
@@ -97,16 +97,11 @@ class Game:
             if isThirty(p1points) return P1res = "Thirty"
             return result = P1res + "-" + P2res
         
-        if (self.p1points > self.p2points and self.p2points >= 3):
-            result = "Advantage " + self.player1Name
+        if (self.p1points > self.p2points and self.p2points >= 3) return result = "Advantage " + self.player1Name
+        if (self.p2points > self.p1points and self.p1points >= 3) return result = "Advantage " + self.player2Name
         
-        if (self.p2points > self.p1points and self.p1points >= 3):
-            result = "Advantage " + self.player2Name
-        
-        if (self.p1points>=4 and self.p2points>=0 and (self.p1points-self.p2points)>=2):
-            result = "Win for " + self.player1Name
-        if (self.p2points>=4 and self.p1points>=0 and (self.p2points-self.p1points)>=2):
-            result = "Win for " + self.player2Name
+        if (self.p1points>=4 and self.p2points>=0 and (self.p1points-self.p2points)>=2) return result = "Win for " + self.player1Name
+        if (self.p2points>=4 and self.p1points>=0 and (self.p2points-self.p1points)>=2) return result = "Win for " + self.player2Name
         return result
     
     def SetP1Score(self, number):
@@ -119,7 +114,6 @@ class Game:
     
     def P1Score(self):
         self.p1points +=1
-    
     
     def P2Score(self):
         self.p2points +=1
